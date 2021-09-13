0b04eec0d94ba9ca0df55c6e9001ae067d79c3ed
Matheus Ramos Kafuri <matheuskafuri@Matheuss-MacBook-Pro.local>
2021-09-13 15:42:13 -0300

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import Logo from '../../assets/logo.svg';
+
+import { Car } from '../../components/Car'; 
+
+import {
+  CarList,
+  Container,
+  Header,
+  HeaderContent,
+  TotalCars,
+} from './styles';
+
+export function Home() {
+  const carData = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'AO DIA',
+      price: 120,
+    },
+    thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png'
+  }
+
+  return (
+    <Container>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo
+            width={RFValue(108)}
+            height={RFValue(12)}
+          />
+          <TotalCars>
+            Total de 12 carros
+          </TotalCars>
+        </HeaderContent>
+      </Header>
+
+      <CarList 
+        data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
+        keyExtractor={item => String(item)}
+        renderItem={({ item }) => <Car data={carData} />}
+      />
+    </Container>
+  );
+}
